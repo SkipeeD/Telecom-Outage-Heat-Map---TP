57a1876fab0c8ee904ae4a0ff9a7166b3acf7e61
Orlando <orlando.dragu05@e-uvt.ro>
2026-04-05 15:17:38 +0300

    feat: Map markers

--- src/components/MapClient.tsx
+++ src/components/MapClient.tsx
@@ -34,10 +34,10 @@ export default function MapClient({ antennas, selectedId, activeFilters, onAnten
       style={{ width: '100%', height: '100%' }}
     >
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        minZoom={0}
-        maxZoom={19}
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        subdomains="abcd"
+        maxZoom={20}
       />
       <MarkerLayer 
         antennas={antennas} 
