4d7a249d78dc8962e7403e830acd3eb15b803801
DanielDunca <daniel.dunca05@e-uvt.ro>
2026-05-05 10:24:34 +0300

    fix: vercel prod

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,12 +1,16 @@
 import { cert, getApps, initializeApp } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 
-if (!getApps().length) {
+function getAdminApp() {
+  if (getApps().length) return getApps()[0]!
+
   const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_JSON
     ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON)
     : process.env.GOOGLE_APPLICATION_CREDENTIALS
 
-  initializeApp({ credential: cert(serviceAccount) })
+  return initializeApp({ credential: cert(serviceAccount) })
 }
 
-export const adminAuth = getAuth()
+export function getAdminAuth() {
+  return getAuth(getAdminApp())
+}
